01fc0f67c617d457983141e2579b803d9269d374
Justin Klaver <Klaverts@yahoo.com>
2022-09-08 14:29:15 -0400

    redo + start page

---
+++ assets/js/questions.js
@@ -0,0 +1,112 @@
+let questions = [
+    {
+    numb: 1,
+    question: "Which is letter a?",
+    answer: "a",
+    options: [
+      "a",
+      "b",
+      "3",
+      "4"
+    ]
+  },
+    {
+    numb: 2,
+    question: "Which is the number 1",
+    answer: "1",
+    options: [
+      "1",
+      "2",
+      "99",
+      "z"
+    ]
+  },
+    {
+    numb: 3,
+    question: "What is today?",
+    answer: "Present",
+    options: [
+      "Past",
+      "Present",
+      "Future",
+      "Tomorrow"
+    ]
+  },
+    {
+    numb: 4,
+    question: "Which is the number 3?",
+    answer: "3",
+    options: [
+      "3",
+      "6",
+      "9",
+      "12"
+    ]
+  },
+    {
+    numb: 5,
+    question: "Which is the number 4?",
+    answer: "4",
+    options: [
+      "4",
+      "44",
+      "444",
+      "4444"
+    ]
+  },
+  {
+    numb: 6,
+    question: "Which is the number 5?",
+    answer: "5",
+    options: [
+      "5",
+      "55",
+      "555",
+      "5555"
+    ]
+  },
+  {
+    numb: 7,
+    question: "Which is the number 6?",
+    answer: "6",
+    options: [
+      "6",
+      "66",
+      "666",
+      "6666"
+    ]
+  },
+  {
+    numb: 8,
+    question: "Which is the number 7?",
+    answer: "7",
+    options: [
+        "7",
+        "77",
+        "777",
+        "7777"
+    ]
+  },
+  {
+    numb: 9,
+    question: "Which is the number 9?",
+    answer: "9",
+    options: [
+        "9",
+        "99",
+        "999",
+        "9999"
+    ]
+  },
+  {
+    numb: 8,
+    question: "Which is the number 8?",
+    answer: "8",
+    options: [
+        "8",
+        "88",
+        "888",
+        "8888"
+    ]
+  },
+];
