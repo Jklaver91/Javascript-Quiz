1eaa843fa0c3a9c66e6610054bf9c1bac6c7b690
Justin Klaver <Klaverts@yahoo.com>
2022-09-24 09:27:44 -0400

    qustions working

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,122 @@
+var ul = document.getElementById('ul')
+var nextButton = document.getElementById('next');
+var quizbox = document.getElementById('question')
+var optionOne = document.getElementById('option1')
+var optionTwo = document.getElementById('option2')
+var optionThree = document.getElementById('option3')
+var optionFour = document.getElementById('option4')
 var timeLeft = 75;
 var timerId;
+
+var app={
+    questions:[
+        {
+            q:'Which is the letter a',
+            options: ['a', 'b', 'c', 'd'],
+            answer:0
+        },
+        {
+            q:'Which is the letter b',
+            options: ['a', 'b', 'c', 'd'],
+            answer:1
+        },
+        {
+            q:'Which is the letter c',
+            options: ['a', 'b', 'c', 'd'],
+            answer:2
+        },
+        {
+            q:'Which is the letter d',
+            options: ['a', 'b', 'c', 'd'],
+            answer:3
+        },
+        {
+            q:'Which is the letter e',
+            options: ['a', 'b', 'c', 'e'],
+            answer:3
+        }            
+    ],
+    index:0,
+
+    load:function(){
+        if(this.index<=this.questions.length-1){
+            question.innerHTML=this.index+1 + ". " +this.questions[this.index].q;
+            optionOne.innerHTML=this.questions[this.index].options[0];
+            optionTwo.innerHTML=this.questions[this.index].options[1];
+            optionThree.innerHTML=this.questions[this.index].options[2];
+            optionFour.innerHTML=this.questions[this.index].options[3];
+        }
+        else {
+            quizbox.innerHTML="Quiz Completed!";
+            ul.classList.add("d-none");
+            nextButton.classList.add("d-none");
+            nextButton.classList.remove("d-flex");
+            endQuiz();
+        }
+    },
+    next: function(){
+        this.index++;
+        this.load();
+    },
+    check: function(ele){
+        var id=ele.id.split("");
+        console.log(id);
+        var id=id.slice(-1);
+        console.log(id);
+        if(id[id.length-1]==this.questions[this.index].answer +1){
+            this.score++;
+            timeLeft = timeLeft + 10;
+            ele.className="btn btn-success d-flex mx-auto mt-4";
+            this.scoreCard();
+        }
+        else{
+            ele.className="btn btn-danger d-flex mx-auto mt-4";
+            timeLeft = timeLeft - 10;
+        }
+    },
+    preventClick:function(){
+        for(let i=0; i<ul.children.length; i++){
+            ul.children[i].style.pointerEvents="none";
+        }
+    },
+    allowClick:function(){
+        for(let i=0; i<ul.children.length; i++){
+            ul.children[i].style.pointerEvents="auto";
+            ul.children[i].className=''
+        }
+    },
+    score:0,
+    scoreCard:function(){
+        marks.innerHTML=this.score + "/" + this.questions.length;
+        percentage.innerHTML= this.score / this.questions.length * 100;
+        
+    }
+}
+
+window.load=app.load();
+
+function button(ele){
+app.check(ele);
+app.preventClick();
+}
+
+function next(){
+app.next();
+app.allowClick();
+}
+
 // ass this class to make invis "d-none"
 
 
 document.getElementById("start").addEventListener("click", function () {
+    startTime();
+    quizCards();
+});
+
+
+function startTime(){
     var timer = document.getElementById('coundownNumber');
     var timerId = setInterval(countdown, 1000);
-    showQuestion(question);
     function countdown() {
         if (timeLeft == -1) {
             endQuiz(timerId);
@@ -15,13 +125,12 @@ document.getElementById("start").addEventListener("click", function () {
             timeLeft--;
         }
     }
-});
+}
 
 function endQuiz() {
     clearInterval(timerId);
     resultCards();
 }
-
 
 function startCards() {
     document.getElementById("start-page").classList.remove("d-none");
@@ -41,3 +150,6 @@ function resultCards() {
     document.getElementById("result").classList.remove("d-none");
 }
 
+document.getElementById("reload").addEventListener("click", function () {
+    window.location.reload();
+});
